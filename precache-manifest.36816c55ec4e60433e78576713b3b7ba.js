740b33202dc658fe5ee69c221762ad1ae5c863d3
tagutaj <tagutaj@gmail.com>
2020-06-21 22:29:49 +0200

    Updates

--- precache-manifest.36816c55ec4e60433e78576713b3b7ba.js
+++ precache-manifest.36816c55ec4e60433e78576713b3b7ba.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a170455586e0450ac818216c1527566",
+    "revision": "8a6f480a32f6e2f48b033437d4753472",
     "url": "/tagutaj.github.io/index.html"
   },
   {
-    "revision": "c5a777a08967c84ef727",
+    "revision": "ca10d238ae4059109d63",
     "url": "/tagutaj.github.io/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "2443ed2c34bbab699dba",
+    "revision": "7a0ca1b04d0fa85ab866",
     "url": "/tagutaj.github.io/static/css/main.11e45714.chunk.css"
   },
   {
-    "revision": "c5a777a08967c84ef727",
-    "url": "/tagutaj.github.io/static/js/2.0fb1340e.chunk.js"
+    "revision": "ca10d238ae4059109d63",
+    "url": "/tagutaj.github.io/static/js/2.e98ff3db.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/tagutaj.github.io/static/js/2.0fb1340e.chunk.js.LICENSE.txt"
+    "url": "/tagutaj.github.io/static/js/2.e98ff3db.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2443ed2c34bbab699dba",
-    "url": "/tagutaj.github.io/static/js/main.067f2012.chunk.js"
+    "revision": "7a0ca1b04d0fa85ab866",
+    "url": "/tagutaj.github.io/static/js/main.98943166.chunk.js"
   },
   {
     "revision": "24ded4a1778890c8f53f",
