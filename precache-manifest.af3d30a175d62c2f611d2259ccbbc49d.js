6b4b00f0cf36339b4db44fe2d5e1b61f23fa926b
tagutaj <tagutaj@gmail.com>
2020-06-21 20:20:38 +0200

    Updates

---
+++ precache-manifest.af3d30a175d62c2f611d2259ccbbc49d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee9b349edc69ac207c5219028d90fd2",
+    "url": "/tagutaj.github.io/index.html"
+  },
+  {
+    "revision": "c5a777a08967c84ef727",
+    "url": "/tagutaj.github.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fe5c35360d3749f52f69",
+    "url": "/tagutaj.github.io/static/css/main.11e45714.chunk.css"
+  },
+  {
+    "revision": "c5a777a08967c84ef727",
+    "url": "/tagutaj.github.io/static/js/2.0fb1340e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tagutaj.github.io/static/js/2.0fb1340e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5c35360d3749f52f69",
+    "url": "/tagutaj.github.io/static/js/main.6840ff6e.chunk.js"
+  },
+  {
+    "revision": "24ded4a1778890c8f53f",
+    "url": "/tagutaj.github.io/static/js/runtime-main.4b3c6570.js"
+  },
+  {
+    "revision": "1d6ff1a0e22551ae2d8fd2ef8d1452bd",
+    "url": "/tagutaj.github.io/static/media/brand.1d6ff1a0.jpg"
+  },
+  {
+    "revision": "69acfb8a25efeabb53421c096ff0267b",
+    "url": "/tagutaj.github.io/static/media/myshop.69acfb8a.mp4"
+  }
+]);
